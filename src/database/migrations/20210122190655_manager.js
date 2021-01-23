eb6610b437bd1012fdaf50d9680683bca174f4d4
Santiagomrn <santiagomrn98@gmail.com>
2021-01-22 20:08:19 -0600

    create doctor validate phone

--- src/database/migrations/20210122190655_manager.js
+++ src/database/migrations/20210122190655_manager.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('manager', t => {
+        t.increments('id').unsigned().primary();
         t.string('email').notNullable();
         t.string('password').notNullable();
         t.timestamps(true,true);
